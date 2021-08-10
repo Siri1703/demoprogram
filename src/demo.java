d702207090b0bff920fb2fb8a6cd29f83e179dff
siri lakshmi <sirilakshmi1703@gmail.com>
2021-08-10 16:01:52 +0530

    commit

---
+++ src/demo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+       ArrayList<String> e = new ArrayList<>();
+        e.add("9");
+        System.out.println(e.size());
+
+    }
+
+}
